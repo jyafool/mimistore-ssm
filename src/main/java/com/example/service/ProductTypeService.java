0faeeff94da76a43cc0023992522c656543effa7
JIA <3086660647@qq.com>
2023-06-28 11:36:00 +0800

    Initial commit

---
+++ src/main/java/com/example/service/ProductTypeService.java
@@ -0,0 +1,11 @@
+package com.example.service;
+
+
+
+import com.example.pojo.ProductType;
+
+import java.util.List;
+
+public interface ProductTypeService {
+    List<ProductType> getAll();
+}
---
+++ src/test/java/com/example/MyTest.java
@@ -0,0 +1,9 @@
+package com.example;
+import org.junit.Test
+public class MyTest {
+    @org.junit.Test
+    public void testMD5(){
+
+    }
+
+}
